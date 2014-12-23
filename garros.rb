a7e53cbfa97a19e469bbd401f494a2b9015a9cd5
FrenchFlakito <guillaume01.simplon@gmail.com>
2014-12-24 00:50:26 +0100

    initial commit

---
+++ garros.rb
@@ -0,0 +1,45 @@
+def score(sequence)
+	score_courant = {:points=>[0, 0],:jeux=>[0, 0],:sets=>[0, 0]}
+
+	points = {J1: 0, J2: 0}
+	jeux = {J1: 0, J2: 0}
+
+
+	sequence.each do |joueur|
+		if situation_d_avantage?(points)
+			points[autre_joueur(joueur)] -= 1
+		else
+			points[joueur] += 1
+		end
+		if situation_gain_jeu?(points.values)
+			points = {J1: 0, J2: 0}
+			jeux = {J1: 1, J2: 0}
+		end
+	end
+
+	score_courant[:points] = jolis_points(points.values)
+	score_courant[:jeux] = jeux.values
+	score_courant
+end
+
+def situation_d_avantage?(points)
+	points[:J1] == 4 && points[:J2] == 3 || 
+	points[:J2] == 4 && points[:J1] == 3
+end
+
+def situation_gain_jeu?(points)
+	puts points.inspect
+	tri_points = points.sort
+	tri_points[1] == 4 && ((tri_points[1] - tri_points[0]) >= 2)
+
+end
+
+def autre_joueur(joueur)
+	return :J1 if joueur == :J2
+	return :J2 if joueur == :J1
+end
+
+def jolis_points(points)
+	points_visuel = [0,15,30,40,"A"]
+	[points_visuel[points[0]], points_visuel[points[1]]]
+end
---
+++ garros_spec.rb
@@ -0,0 +1,35 @@
+require 'garros'
+
+describe "Garros" do
+
+	describe :score do  
+	  	it { expect(score([])).to eq({:points=>[0,0],:jeux=>[0,0],:sets=>[0,0]}) }
+	  	it { expect(score([:J1])).to eq({:points=>[15,0],:jeux=>[0,0],:sets=>[0,0]}) }
+	  	it { expect(score([:J1, :J1])).to eq({:points=>[30,0],:jeux=>[0,0],:sets=>[0,0]}) }
+	  	it { expect(score([:J1, :J1, :J2])).to eq({:points=>[30,15],:jeux=>[0,0],:sets=>[0,0]}) }
+	  	it { expect(score([:J1, :J1, :J1, :J2, :J2, :J2, :J1])).to eq({:points=>["A",40],:jeux=>[0,0],:sets=>[0,0]}) }
+	  	it { expect(score([:J1, :J1, :J1, :J2, :J2, :J2, :J1, :J2])).to eq({:points=>[40,40],:jeux=>[0,0],:sets=>[0,0]}) }
+	  	it { expect(score([:J1, :J1, :J1, :J2, :J2, :J2, :J1, :J2, :J1])).to eq({:points=>["A",40],:jeux=>[0,0],:sets=>[0,0]}) }
+	  	it { expect(score([:J1, :J1, :J1, :J1])).to eq({:points=>[0,0],:jeux=>[1,0],:sets=>[0,0]}) }
+	 end
+
+  	describe :jolis_points do
+  		it { expect(jolis_points([2,3])).to eq([30,40]) }
+  		it { expect(jolis_points([1,0])).to eq([15,0]) }
+  		it { expect(jolis_points([4,3])).to eq(["A",40]) }
+  	end
+
+  	describe :autre_joueur do
+  		it { expect(autre_joueur(:J1)).to eq(:J2) }
+  		it { expect(autre_joueur(:J2)).to eq(:J1) }
+  	end
+
+  	describe :situation_gain_jeu? do
+		it { expect(situation_gain_jeu?([4,0])).to eq(true) }
+		it { expect(situation_gain_jeu?([3,0])).to eq(false) }
+		it { expect(situation_gain_jeu?([4,3])).to eq(false) }
+		it { expect(situation_gain_jeu?([2,2])).to eq(false) }
+  	end
+
+
+end
